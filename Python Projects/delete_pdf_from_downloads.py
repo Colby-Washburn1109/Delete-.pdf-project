e2b806c3986fa6e00401bc6d4b13a39398ff5eb5
Colby-Washburn1109 <132858304+Colby-Washburn1109@users.noreply.github.com>
2023-12-16 17:41:40 -0500

    Add files via upload

---
+++ Python Projects/delete_pdf_from_downloads.py
@@ -0,0 +1,28 @@
+## This is code to clean out my downloads folder using the os module ## 
+
+# Import os module, to allow interaction in operating system
+import os
+
+# opens the directory to my downloads folder
+directory_path = '/users/Colby/Downloads'
+
+files_deleted = False
+
+print("The Pdf Files in ", directory_path)
+
+# A loop that returns the entries in my Downloads folder
+# Then joins the name of the file specifically in the Downloads folder
+# Uses os.remove to delete the file
+
+for filename in os.listdir(directory_path):
+    file_path = os.path.join(directory_path, filename)
+    
+    if file_path.lower().endswith('.pdf') and os.path.isfile(file_path):
+        os.remove(file_path)
+        files_deleted = True
+        print(f"Deleted: {file_path}")
+
+if files_deleted:
+    print("Files have been deleted")
+else:
+    print('No files have been deleted')
